43e7f5e9bcb4953d81d21cfbbddf29f8ba203c13
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-05 20:51:02 -0300

    feat: add user register route

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,36 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const passwordHash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    reply.status(409).send()
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash,
+    },
+  })
+
+  return reply.status(201).send()
+}
