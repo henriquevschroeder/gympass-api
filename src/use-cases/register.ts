6447035077c6a25761f73094c814632f08621aae
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-05 21:13:35 -0300

    refactor: create user repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseParams {
@@ -7,28 +7,22 @@ interface RegisterUseCaseParams {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseParams) {
-  const passwordHash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+  async execute({ name, email, password }: RegisterUseCaseParams) {
+    const passwordHash = await hash(password, 6)
 
-  if (userWithSameEmail) {
-    throw new Error('User with same email already exists')
-  }
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-  await prisma.user.create({
-    data: {
+    if (userWithSameEmail) {
+      throw new Error('User with same email already exists')
+    }
+
+    await this.usersRepository.create({
       name,
       email,
       passwordHash,
-    },
-  })
+    })
+  }
 }
