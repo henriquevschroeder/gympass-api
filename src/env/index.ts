76c41b4c48c764fe8256da9cf6083ed8fde10915
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-18 19:53:32 -0300

    feat: add jwt generation for the authentication

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().min(1),
+  JWT_SECRET: z.string().min(1),
 })
 
 const _env = envSchema.safeParse(process.env)
