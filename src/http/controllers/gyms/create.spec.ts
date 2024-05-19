7e273cfa05cad895383c1665cbe68e46780b8358
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-18 23:14:18 -0300

    test(e2e): add end-to-end test for creating a gym

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Konoha Academy',
+        description: 'Located in Leaf Village',
+        phone: '123456789',
+        latitude: 35.6684103,
+        longitude: 139.5760607,
+      })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
