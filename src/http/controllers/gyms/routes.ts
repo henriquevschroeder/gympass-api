d16c22ec67abc031ab746780497938edccd9e087
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-18 21:22:31 -0300

    feat: add fetch nearby gyms endpoint

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
+import { nearby } from './nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/gyms', create)
+  app.get('/gyms/nearby', nearby)
 }
