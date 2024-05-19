092f726c09b7bcd38f11d5c31583ea6b9568b33c
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-18 21:13:41 -0300

    refactor: move users routes to separated folder

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify'
-import { appRoutes } from './http/routes'
+import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
@@ -10,7 +10,7 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
 
 app.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
