ba6debe291990356b31643c25d7c9c006f91d95f
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-18 23:03:57 -0300

    feat: add check-in history endpoint for users

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
 import { validate } from './validate'
+import { history } from './history'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
+  app.get('/check-ins/history', history)
   app.post('/gyms/:gymId/check-in', create)
   app.patch('/check-ins/:checkInId/validate', validate)
 }
