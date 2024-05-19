318e58a49eb7b8c9c2cacdcb6287c779401b37d3
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-18 22:10:25 -0300

    feat: add check-in validation endpoint for gyms

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
+import { validate } from './validate'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/gyms/:gymId/check-in', create)
+  app.patch('/gyms/:gymId/check-in', validate)
 }
