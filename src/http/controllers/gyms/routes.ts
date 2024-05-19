d87c2c9f793fb11da70c425032ba299be166d7fd
henriquevschroeder <henriquevschdev@gmail.com>
2024-05-18 21:22:59 -0300

    feat: add gyms search endpoint

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
 import { nearby } from './nearby'
+import { search } from './search'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.post('/gyms', create)
   app.get('/gyms/nearby', nearby)
+  app.get('/gyms/search', search)
 }
